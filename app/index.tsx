8b8e12b0d9372a11116492dbaf833e03ce77a323
Ayush Yadav <sidharth197502@gmail.com>
2025-09-13 13:30:01 +0530

    basic skelton of the app

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import "../global.css";
+export default function Index() {
+  return (
+    <View className="justify-center  items-center flex-1">
+      <Text className=" text-red-500 font-bold text-2xl ">
+        Welcome to Raksha 360{" "}
+      </Text>
+    </View>
+  );
+}
